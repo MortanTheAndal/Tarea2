3cdd12d3576c46ec728d7543166e5cb6130cbd98
MortanTheAndal <martinr769@gmail.com>
2018-06-06 13:09:51 -0400

    Data Generator Created

---
+++ src/tarea2/DataGenerator.java
@@ -0,0 +1,26 @@
+package tarea2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+public class DataGenerator {
+    
+    
+    public DataGenerator(){
+	
+    }
+    
+    public ArrayList<Integer> getData(int k){
+	ArrayList<Integer> data = new ArrayList<Integer>();
+	for (int i = 1; i <= Math.pow(2, k); i++){
+	    data.add(i);
+	}
+	Collections.shuffle(data);
+	
+	return data; 
+    }
+    
+    public static void main(String[] args){
+	DataGenerator dg = new DataGenerator();
+	System.out.println(dg.getData(4));
+    }
+}
