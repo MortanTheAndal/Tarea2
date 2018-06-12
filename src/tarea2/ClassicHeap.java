1efbdf5620aae7505f1541e13c2652c7cc47bd58
Luis <lpbustoscarrasco@gmail.com>
2018-06-12 17:22:02 -0400

    Several modifications to heapify function in ClassicHeap. Add helper functions to ClassicHeap to build the heap from an array. Restored Builder in order to create a ClassicHeap from a Node ArrayList

--- src/tarea2/ClassicHeap.java
+++ src/tarea2/ClassicHeap.java
@@ -2,11 +2,18 @@ package tarea2;
 import java.util.ArrayList;
 
 public class ClassicHeap implements PriorityQueue{
-    private ArrayList<Node> heap;
-    private int heap_length;
+    public ArrayList<Node> heap;
+    public int heap_length;
+
     public ClassicHeap(){
          this.heap = new ArrayList<>();
          this.heap_length =  heap.size();
+    }
+
+    public ClassicHeap(ArrayList<Node> list){
+        this.heap = list;
+        this.heap_length = list.size();
+        this.heap = this.heapify();
     }
 
     @Override
@@ -20,48 +27,58 @@ public class ClassicHeap implements PriorityQueue{
     private void emerger(ArrayList<Node> heap) {
         for (int j = heap_length-1; j>1 && heap.get(j).prioridad > heap.get(j/2).prioridad; j /= 2){
             Node temp = heap.get(j);
-            heap.add(j, heap.get(j/2));
-            heap.add(j/2, temp);
+            heap.set(j, heap.get(j/2));
+            heap.set(j/2, temp);
         }
     }
 
     @Override
     public int extraer_siguiente() {
+        if (this.heap_length == 1){
+            int result = this.heap.get(0).valor;
+            this.heap.remove(this.heap_length- 1);
+            this.heap_length--;
+            return result;
+        }
         Node first = this.heap.get(0);
         Node last = this.heap.get(this.heap_length - 1);
         this.heap.remove(this.heap_length- 1);
-        this.heap.add(0, last);
-        this.heap_length =  heap.size();
-        sumerger(this.heap);
+        this.heap.set(0, last);
+        this.heap_length--;
+        buildHeap(0);
         return first.valor;
     }
 
-    private void sumerger(ArrayList<Node> heap) {
-        int j = 0;
-        while(2*j <= this.heap_length-1){
-            int k = 2*j;
-            if(k+1 <= heap_length-1 && heap.get(k+1).prioridad > heap.get(k).prioridad){
-                k += 1;
-            }
-            if (heap.get(j).prioridad > heap.get(k).prioridad){
-                break;
-            }
-            Node temp = heap.get(j);
-            heap.add(j, heap.get(k));
-            heap.add(k, temp);
-            j=k;
+    public ArrayList<Node> meld(ClassicHeap c1, ClassicHeap c2) {
+        c1.heap.addAll(c2.heap);
+        return this.heapify();
+    }
+
+    private ArrayList<Node> heapify(){
+        for(int i = this.heap.size()/2 -1; i>=0;i--){
+            buildHeap(i);
+        }
+        return this.heap;
+    }
+    private void buildHeap(int index) {
+        int largest = index;
+        int leftIndex = 2 * index + 1;
+        int rightIndex = 2 *index + 2;
+        if(leftIndex < this.heap_length && this.heap.get(index).prioridad <this.heap.get(leftIndex).prioridad){
+            largest = leftIndex;
+        }
+        if(rightIndex < this.heap_length && this.heap.get(index).prioridad <this.heap.get(rightIndex).prioridad){
+            largest = rightIndex;
+        }
+        if(largest != index){
+            swap(index,largest);
+            buildHeap(largest);
         }
     }
 
-    public ClassicHeap meld(ClassicHeap c1, ClassicHeap c2) {
-        c1.heap.addAll(c2.heap);
-        return this.heapify(c1.heap);
-    }
-
-    public ClassicHeap heapify(ArrayList<Node> list) {
-        this.heap = list;
-        this.heap_length = list.size();
-
-        return this;
+    private void swap(int first, int second){
+        Node temp = this.heap.get(first);
+        heap.set(first, heap.get(second));
+        heap.set(second, temp);
     }
 }
--- src/test/ClassicHeapTest.java
+++ src/test/ClassicHeapTest.java
@@ -7,6 +7,7 @@ import tarea2.ClassicHeap;
 import tarea2.DataGenerator;
 import tarea2.Node;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
@@ -24,7 +25,7 @@ public class ClassicHeapTest extends TestCase {
     }
     @Test
     public void testSort() throws FileNotFoundException, UnsupportedEncodingException {
-        writer = new PrintWriter("Results/ClassicHeap_Sorting.csv", "UTF-8");
+        writer = new PrintWriter("Results/ClassicHeap_Sorting.csv");
         for(int i = 15; i<=21; i++){
             ArrayList<Integer> data = generator.getData(i);
             ArrayList<Node> nodeList = new ArrayList<Node>();
@@ -32,23 +33,33 @@ public class ClassicHeapTest extends TestCase {
                 Node to_insert = new Node(j,j);
                 nodeList.add(to_insert);
             }
+            System.out.println("Start Heapify");
             long heapify_start = System.currentTimeMillis() % 1000;
-            ClassicHeap heap = heap_a.heapify(nodeList);
+            System.out.println(nodeList.size());
+            ClassicHeap heap = new ClassicHeap(nodeList);
+            System.out.println(heap.heap_length);
             long heapify_finish = System.currentTimeMillis() % 1000;
+            System.out.println("End Heapify");
             ArrayList<Integer> sorted_data = new ArrayList<Integer>();
+            System.out.println("Start Sorting");
             long extraction_start = System.currentTimeMillis() % 1000;
-            for(int k =0 ; k< nodeList.size();k++){
+            int length = heap.heap_length;
+            for(int k =0 ; k< length;k++){
                 sorted_data.add(heap.extraer_siguiente());
             }
+            System.out.println(nodeList.size());
             long extraction_finish = System.currentTimeMillis() % 1000;
+            System.out.println("Stop Sorting");
+            System.out.println(sorted_data.size());
+            assertEquals(0, heap.heap_length);
             writer.println(i +";"+ (heapify_finish - heapify_start) + ";" + (extraction_finish - extraction_start));
         }
+        writer.close();
     }
 
     @Test
     public void testInsertionAndMelding() throws FileNotFoundException, UnsupportedEncodingException {
-        writer = new PrintWriter("Results/ClassicHeap_InsertionAndMelding.csv", "UTF-8");
-
+        //writer = new PrintWriter("Results/ClassicHeap_InsertionAndMelding.csv", "UTF-8");
     }
 
 
