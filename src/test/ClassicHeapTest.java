9b659d0d1898a77781517cadd1af803f578069a8
Luis <lpbustoscarrasco@gmail.com>
2018-06-14 18:43:58 -0400

    Add Insertion and Melding test

--- src/test/ClassicHeapTest.java
+++ src/test/ClassicHeapTest.java
@@ -14,15 +14,14 @@ import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 
 public class ClassicHeapTest extends TestCase {
-    DataGenerator generator;
-    ClassicHeap heap_a;
-    PrintWriter writer;
+    private DataGenerator generator;
+    private PrintWriter writer;
     @Before
     public void setUp() throws Exception {
         super.setUp();
         generator = new DataGenerator();
-        heap_a = new ClassicHeap();
     }
+
     @Test
     public void testSort() throws FileNotFoundException, UnsupportedEncodingException {
         writer = new PrintWriter("Results/ClassicHeap_Sorting.csv");
@@ -56,8 +55,43 @@ public class ClassicHeapTest extends TestCase {
 
     @Test
     public void testInsertionAndMelding() throws FileNotFoundException, UnsupportedEncodingException {
-        //writer = new PrintWriter("Results/ClassicHeap_InsertionAndMelding.csv", "UTF-8");
+        writer = new PrintWriter("Results/ClassicHeap_InsertionAndMelding.csv", "UTF-8");
+        ArrayList<Integer> n = generator.getData(20);
+        for(int i=0;i<16;i++){
+            ArrayList<ClassicHeap> heap_list=  new ArrayList<ClassicHeap>();
+            //TODO: Insercion y melding
+            int n_of_heaps = (int) Math.pow(2,20-i);
+            int index = 0;
+            System.out.println("Start insertions");
+            long insertion_start = System.currentTimeMillis()%1000;
+            for(int j = 0; j<n_of_heaps; j++){
+                ClassicHeap heap = new ClassicHeap();
+                for(int k=0; k < Math.pow(2,i);k++){
+                    int to_insert = n.get(index);
+                    heap.insertar(to_insert,to_insert);
+                    index++;
+                }
+                heap_list.add(heap);
+            }
+
+            long insertion_finish = System.currentTimeMillis()%1000;
+            System.out.println("Finish insertions");
+            System.out.println("Start meldings");
+            long melding_start = System.currentTimeMillis()%1000;
+            while (heap_list.size() > 1){
+                ArrayList<ClassicHeap> aux = new ArrayList<ClassicHeap>();
+                for(int j=0; j<heap_list.size(); j=j+2){
+                    ClassicHeap heap = new ClassicHeap();
+                    heap.meld(heap_list.get(j),heap_list.get(j+1));
+                    aux.add(heap);
+                }
+                heap_list = aux;
+            }
+            long melding_finish = System.currentTimeMillis()%1000;
+            System.out.println("Finish meldings");
+            assertEquals(1, heap_list.size());
+            writer.println(i +";"+ (insertion_finish - insertion_start) + ";" + (melding_finish - melding_start));
+        }
+        writer.close();
     }
-
-
 }
