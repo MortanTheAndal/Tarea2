15056ac251ebadaed9f9452c511b4298c1d397c7
Luis <lpbustoscarrasco@gmail.com>
2018-07-07 17:32:10 -0400

    Moved files to the tarea2package, deleted tests, moved tests to experiments class

--- src/tarea2/Experiments.java
+++ src/tarea2/Experiments.java
@@ -201,6 +201,134 @@ public class Experiments {
         writer.close();
     }
 
+    private void testInsertionAndMelding_BinHeap() throws FileNotFoundException, UnsupportedEncodingException {
+        writer = new PrintWriter("Results/BinHeap_InsertionAndMelding.csv", "UTF-8");
+        ArrayList<Integer> n = generator.getData(20);
+        for (int a = 0; a <= 15; a++) {
+            for (int i = 0; i < 16; i++) {
+                ArrayList<BinHeap> heap_list = new ArrayList<BinHeap>();
+                int n_of_heaps = (int) Math.pow(2, 20 - i);
+                int index = 0;
+                System.out.println("Start insertions " + a + ":" + i);
+                long insertion_start = System.nanoTime();
+                for (int j = 0; j < n_of_heaps; j++) {
+                    BinHeap heap = new BinHeap();
+                    for (int k = 0; k < Math.pow(2, i); k++) {
+                        int to_insert = n.get(index);
+                        heap.insertar(to_insert, to_insert);
+                        index++;
+                    }
+                    heap_list.add(heap);
+                }
+
+                long insertion_finish = System.nanoTime();
+                long melding_start = System.nanoTime();
+                while (heap_list.size() > 1) {
+                    ArrayList<BinHeap> aux = new ArrayList<BinHeap>();
+                    for (int j = 0; j < heap_list.size(); j = j + 2) {
+                        BinHeap heap = BinHeap.meld(heap_list.get(j), heap_list.get(j + 1));
+                        aux.add(heap);
+                    }
+                    heap_list = aux;
+                }
+                long melding_finish = System.nanoTime();
+                writer.println(i + ";" + (insertion_finish - insertion_start) + ";" + (melding_finish - melding_start));
+            }
+        }
+
+        writer.close();
+    }
+
+    private void testSort_BinHeap() throws FileNotFoundException, UnsupportedEncodingException {
+        writer = new PrintWriter("Results/BinHeap_Sorting.csv", "UTF-8");
+        for (int a = 0; a <= 15; a++) {
+            for (int i = 15; i <= 21; i++) {
+                ArrayList<Integer> data = generator.getData(i);
+                BinHeap heap = new BinHeap();
+                long heapify_start = System.nanoTime();
+                System.out.println(data.size());
+                for (int j : data) {
+                    heap.insertar(j, j);
+                }
+                long heapify_finish = System.nanoTime();
+                ArrayList<Integer> sorted_data = new ArrayList<Integer>();
+                long extraction_start = System.nanoTime();
+                int length = heap.size;
+                for (int k = 0; k < length; k++) {
+                    sorted_data.add(heap.extraer_siguiente());
+                }
+                long extraction_finish = System.nanoTime();
+                writer.println(
+                        i + ";" + (heapify_finish - heapify_start) + ";" + (extraction_finish - extraction_start));
+            }
+        }
+
+        writer.close();
+    }
+
+    private void testSort_FibHeap() throws FileNotFoundException, UnsupportedEncodingException {
+        writer = new PrintWriter("Results/FibHeap_Sorting.csv", "UTF-8");
+        for (int a = 0; a <= 15; a++) {
+
+            for (int i = 15; i <= 21; i++) {
+                ArrayList<Integer> data = generator.getData(i);
+                FibHeap heap = new FibHeap();
+                long heapify_start = System.nanoTime();
+                for (int j : data) {
+                    heap.insertar(j, j);
+                }
+
+                long heapify_finish = System.nanoTime();
+                ArrayList<Integer> sorted_data = new ArrayList<Integer>();
+                long extraction_start = System.nanoTime();
+                int length = heap.size;
+                for (int k = 0; k < length; k++) {
+                    sorted_data.add(heap.extraer_siguiente());
+                }
+                long extraction_finish = System.nanoTime();
+                writer.println(
+                        i + ";" + (heapify_finish - heapify_start) + ";" + (extraction_finish - extraction_start));
+            }
+        }
+        writer.close();
+    }
+
+    private void testInsertionAndMelding_FibHeap() throws FileNotFoundException, UnsupportedEncodingException {
+        writer = new PrintWriter("Results/FibHeap_InsertionAndMelding.csv", "UTF-8");
+        ArrayList<Integer> n = generator.getData(20);
+        for (int a = 0; a <= 15; a++) {
+            for (int i = 0; i < 16; i++) {
+                ArrayList<FibHeap> heap_list = new ArrayList<FibHeap>();
+                int n_of_heaps = (int) Math.pow(2, 20 - i);
+                int index = 0;
+                long insertion_start = System.nanoTime();
+                for (int j = 0; j < n_of_heaps; j++) {
+                    FibHeap heap = new FibHeap();
+                    for (int k = 0; k < Math.pow(2, i); k++) {
+                        int to_insert = n.get(index);
+                        heap.insertar(to_insert, to_insert);
+                        index++;
+                    }
+                    heap_list.add(heap);
+                }
+
+                long insertion_finish = System.nanoTime();
+                long melding_start = System.nanoTime();
+                while (heap_list.size() > 1) {
+                    ArrayList<FibHeap> aux = new ArrayList<FibHeap>();
+                    for (int j = 0; j < heap_list.size(); j = j + 2) {
+                        FibHeap heap = FibHeap.meld(heap_list.get(j), heap_list.get(j + 1));
+                        aux.add(heap);
+                    }
+                    heap_list = aux;
+                }
+                long melding_finish = System.nanoTime();
+                writer.println(i + ";" + (insertion_finish - insertion_start) + ";" + (melding_finish - melding_start));
+            }
+        }
+
+        writer.close();
+    }
 
     public static void main(String args[]) throws FileNotFoundException, UnsupportedEncodingException{
         Experiments experiments = new Experiments();
@@ -210,5 +338,9 @@ public class Experiments {
         experiments.testSort_LeftistTree();
         experiments.testInsertionAndMelding_SkewHeap();
         experiments.testSort_SkewHeap();
+        experiments.testInsertionAndMelding_BinHeap();
+        experiments.testSort_BinHeap();
+        experiments.testInsertionAndMelding_FibHeap();
+        experiments.testSort_FibHeap();
     }
 }
---
+++ src/tarea2/FNode.java
@@ -0,0 +1,15 @@
+package tarea2;
+
+public class FNode {
+	
+	FNode left, right, child, parent;
+	int elem, rank, prio; 
+	
+	public FNode(int e, int p) {
+		elem= e;
+		prio=p;
+		left = this;
+		right=this;
+	}
+
+}
